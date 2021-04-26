aa38167c6e7cde26b5c5dd896c0c3cca6f7cd0aa
maxime Glaizot <maxime@glaizot.net>
2021-04-26 12:38:54 +0200

    modif

---
+++ Trap2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Trap.sh : le piège, gentil... 
+
+REPERTOIRE_SCRIPT=$(cd $( dirname ${BASH_SOURCE[0]}) && pwd)
+trap "" SIGINT SIGTERM
+trap "" SIGTSTP 
+#echo "pid is $$"
+
+
+
+while :			# This is the same as "while true".
+do
+        echo "tu es prisonier a jamais !!! ;) ;) ;) "
+        sleep 4 	
+done
