bea9968845156c03954749c6db079df91ef51c12
Maxime <maxime@glaizot.net>
2021-04-26 09:57:05 +0200

    ajout lien

--- CreateFile.sh
+++ CreateFile.sh
@@ -8,8 +8,8 @@ fi
 
 for nom in $listeEtudiant
 do
-    echo $nom > $repertoireTest/$nom.txt
     echo $nom > $repertoireTest/$nom.id 
+    ln -s $repertoireTest/$nom.id $repertoireTest/$nom.txt 
 done
 
 if [ ! -d $documents ];  then
@@ -20,4 +20,5 @@ echo $listeEtudiant > $repertoireTest/listecomplete\?.txt
 mkdir $repertoireTest/ana\&luc
 mkdir $documents/seb\&lola
 touch $documents/seb\&lola/\ grrrr.~old
-
+echo "maxime" > $repertoireTest/LeProf.id
+ln $repertoireTest/LeProf.id $repertoireTest/LeProf.txt  
